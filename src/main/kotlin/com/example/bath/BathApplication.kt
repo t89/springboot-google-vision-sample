2b0139ca192f5b43395e10ad3590f129e0d09ca0
Thomas Johannesmeyer <thomas@geeky.gent>
2020-04-06 08:50:48 -0500

    Turn of banner mode

--- src/main/kotlin/com/example/bath/BathApplication.kt
+++ src/main/kotlin/com/example/bath/BathApplication.kt
@@ -1,5 +1,6 @@
 package com.example.bath
 
+import org.springframework.boot.Banner
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
@@ -7,5 +8,7 @@ import org.springframework.boot.runApplication
 class BathApplication
 
 fun main(args: Array<String>) {
-	runApplication<BathApplication>(*args)
+	runApplication<BathApplication>(*args) {
+		setBannerMode(Banner.Mode.OFF)
+	}
 }
