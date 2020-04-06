eb6b5d7504be451e2b7eedbf39adb689896de858
Thomas Johannesmeyer <thomas@geeky.gent>
2020-04-06 08:23:19 -0500

    Add clean state

---
+++ src/test/kotlin/com/example/bath/BathApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.bath
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BathApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
